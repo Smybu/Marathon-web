5bd488e835fab29c4bf33ad6b45e91e369b3f082
Goudaillier Antoine <antoine_goudaillier@ens.univ-artois.fr>
2023-12-20 17:12:20 +0100

    ajout footer

--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>{{isset($title) ? $title : "Page en cours"}}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite(["resources/css/normalize.css", 'resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
@@ -50,6 +50,67 @@
     @yield("content")
 </main>
 
-<footer>IUT de Lens</footer>
+<footer>
+    <!--
+    <div class="container-footer">
+        <div class="box">
+            <ul><h1 class="titre-footer">A propos</h1>
+
+                <li><a href="#" class="texte-footer">Qui sommes nous ?</a></li>
+                <li><a href="#" class="texte-footer">Mentions légales</a></li>
+            </ul>
+        </div>
+        <div class="box">
+            <ul><h1 class="titre-footer">plan du site</h1>
+
+            <li><a href="#" class="texte-footer">Accueil</a></li>
+            <li><a href="#" class="texte-footer">Histoires</a></li>
+            <li><a href="#" class="texte-footer">Mes histoires</a></li>
+            <li><a href="#" class="texte-footer">Login / Register</a></li>
+            </ul>
+        </div>
+        <div class="box">
+            <ul><h1 class="titre-footer">Contact</h1>
+                <div class="logos-rs">
+                    <a href="#" class="texte-footer"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#" class="texte-footer"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#" class="texte-footer"><i class="fa-brands fa-twitter"></i></a>
+                </div>
+            </ul>
+        </div>
+    </div>
+    -->
+    <div class="container-footer">
+        <div class="box">
+            <h1 class="titre-footer">A propos</h1>
+            <ul>
+                <!-- Ajouter les routes-->
+                <li><a href="#" class="texte-footer">Qui sommes nous ?</a></li>
+                <li><a href="#" class="texte-footer">Mentions légales</a></li>
+            </ul>
+        </div>
+        <div class="box">
+            <h1 class="titre-footer">plan du site</h1>
+            <ul>
+                <!-- Ajouter les routes-->
+                <li><a href="#" class="texte-footer">Accueil</a></li>
+                <li><a href="#" class="texte-footer">Histoires</a></li>
+                <li><a href="#" class="texte-footer">Mes histoires</a></li>
+                <li class="texte-footer"><a href="#">Login</a> / <a href="#">Register</a></li>
+            </ul>
+        </div>
+        <div class="box">
+            <h1 class="titre-footer">Contact</h1>
+            <ul>
+                <div class="logos-rs">
+                    <a href="#" class="texte-footer"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#" class="texte-footer"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#" class="texte-footer"><i class="fa-brands fa-twitter"></i></a>
+                </div>
+            </ul>
+        </div>
+    </div>
+
+</footer>
 </body>
 </html>
