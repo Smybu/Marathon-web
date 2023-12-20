152859402fd50e146a26d7314d41e17046334fd9
Esteban DAUCHY <esteban_dauchy@ens.univ-artois.fr>
2023-12-20 13:36:11 +0100

    ajout section pour css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,24 +1,25 @@
 @extends("templates.app")
 
 @section('content')
-    <div style="display: flex;align-items: center; justify-content: center">
-        <div class="main-title">
-            <h1>Bienvenue dans le Panthéon</h1>
+    <section id="accueil">
+        <div style="display: flex;align-items: center; justify-content: center">
+            <div class="main-title">
+                <h1>Bienvenue dans le Panthéon</h1>
+            </div>
+            <div class="second-title">
+                <h2>Voici quelques histoires :</h2>
+            </div>
+
+            <div class="history">
+                @if(!empty($histoires))
+
+                    @foreach($histoires as $histoire)
+                        <h3><a href="#">{{$histoire['titre']}}</a></h3>
+                        <h4>{{$histoire['pitch']}}</h4>
+                    @endforeach
+
+                @endif
+            </div>
         </div>
-        <div class="second-title">
-            <h2>Voici quelques histoires :</h2>
-        </div>
-
-        <div class="history">
-            @if(!empty($histoires))
-
-                @foreach($histoires as $histoire)
-                    <h3><a href="#">{{$histoire['titre']}}</a></h3>
-                    <h4>{{$histoire['pitch']}}</h4>
-                @endforeach
-
-            @endif
-        </div>
-    </div>
-
+    </section>
 @endsection
