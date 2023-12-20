52d1871ac0ebbe938f8f49f93d550d3ef1c0338e
Batiste0912 <batiste.dubo@outlook.fr>
2023-12-20 14:36:55 +0100

    Ajout de la page show de l'histoire

---
+++ resources/views/histoire/show.blade.php
@@ -0,0 +1,37 @@
+@extends('templates.app')
+
+@section('title', $histoire->titre)
+
+@section('content')
+<section id="histoire_show">
+    @if($histoire->active || auth()->id() == $histoire->user_id)
+        <div id="head_histoire">
+            <img src="{{Storage::url($histoire->photo)}}" alt="Image {{$histoire->titre}}">
+            <h1>{{$histoire->titre}}</h1>
+        </div>
+
+        <div id="resume">
+            <h3>Résumé :</h3>
+            <p>{{$histoire->pitch}}</p>
+        </div>
+
+        <p2>Histoire écrite par : {{$histoire->user->name}}</p>
+
+        <p>Cette histoire a été terminée {{$histoire->terminees()->count()}} fois</p>
+
+        <a href="#">Commencer l'aventure</a>
+
+        <div id="avis">
+            <h3>Avis :</h3>
+            @foreach($histoire->avis as $avis)
+                <div class="comment">
+                <p>{{$avis->contenu}}</p>
+                <p>Ajouté le : {{$avis->created_at}}</p>
+                </div>
+            @endforeach
+        </div>
+    @else
+        <h1>Histoire en construction...</h1>
+    @endif
+</section>
+@endsection
