4accebff25f9f569d1af91be110f0ff90e3a4212
thomashazebrouck <hazebrouck.thomas@hotmail.fr>
2023-12-21 14:19:39 +0100

    Ajout page somme nous et photo de nous

--- resources/views/whoweare.blade.php
+++ resources/views/whoweare.blade.php
@@ -5,9 +5,8 @@
     <div class='whoweare-fond'>
         <div class="whoweare-container">
             <H1>Qui sommes nous ?</H1>
-                <img src="../images/surfer.jpg"  alt=TeamPenthéon></a></p>
+                <img src="{{Vite::asset('resources/images/nous.jpg')}}"  alt=TeamPenthéon></img>
                 <p>Nous sommes 4 étudiants en BUT MMI et 4 étudiants en BUT Informatique. Nous avons créé Le Panthéon pour vous permettre de vivre une expérience d’histoires interactives dans le thème de la mythologie. Ce site est à destination des personnes de tout âge et de tous horizons.</p>
         </div>
     </div>
-
 @endsection
