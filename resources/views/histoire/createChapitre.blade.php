1c46bc7e63f7f6bf211b08c8d244f3e7140c8443
Goudaillier Antoine <antoine_goudaillier@ens.univ-artois.fr>
2023-12-21 15:12:08 +0100

    ajout pages

--- resources/views/histoire/createChapitre.blade.php
+++ resources/views/histoire/createChapitre.blade.php
@@ -1,47 +1,54 @@
-<form action="{{route('chapitre.store')}}" method="POST">
-    {!! csrf_field() !!}
-    <input type="hidden" name="histoire_id" value="{{$id}}">
-    <div class="text-center" style="margin-top: 2rem">
-        <h3>Création d'un chapitre</h3>
+<section class="section-createChapitre">
+    <form action="{{route('chapitre.store')}}" method="POST">
+        {!! csrf_field() !!}
+        <input type="hidden" name="histoire_id" value="{{$id}}">
 
-    </div>
-    <div>
+            <h1 class="titre-createChapitre">Création d'un chapitre</h1>
 
-        <label for="titre"><strong>Titre : </strong></label>
-        <input type="text" name="titre" id="titre"
-               value="{{ old('titre') }}"
-               placeholder="Ex : Chap 1 : La rencontre">
-    </div>
-    <div>
-        <label for="titrecout"><strong>Titre court : </strong></label>
-        <input type="text" class="form-control" id="titrecout" name="titrecourt"
-               value="{{ old('titrecourt') }}">
-    </div>
-    <div>
-        <label for="texte"><strong>Texte : </strong></label>
-        <input type="text" name="texte" id="texte"
-               value="{{ old('texte') }}"
-               placeholder="Le texte lié au chapitre">
-    </div>
-    <div>
-        <label for="media"><strong>Media</strong></label>
-        <input type="text" name="media" id="media"
-               value="{{ old('media') }}"
-               placeholder="Média lié au chapitre">
-    </div>
-    <div>
-        <label for="question"><strong>Question</strong></label>
-        <input type="text" name="question" id="question"
-               value="{{ old('question') }}"
-               placeholder="Question liée au chapitre">
-    </div>
-    @if(!isset($affichePremier))
-        <div>
-            <label for="premier"><strong>Premier chapitre?</strong></label>
-            <input type="checkbox" name="premier" checked id="premier">
+            <div class="input-enLigne-createChapitre">
+                <label for="titre">Titre</label>
+                <input type="text" name="titre" id="titre" value="{{ old('titre') }}" placeholder="Ex : Chap 1 : La rencontre">
+            </div>
+
+            <div class="input-enLigne-createChapitre">
+                <label for="titrecout">Titre court</label>
+                <input type="text" class="form-control" id="titrecout" name="titrecourt"
+                       value="{{ old('titrecourt') }}">
+            </div>
+
+            <div class="input-enLigne-createChapitre">
+            <label for="texte">Texte</label>
+            <input type="text" name="texte" id="texte"
+                   value="{{ old('texte') }}"
+                   placeholder="Le texte lié au chapitre">
+            </div>
+
+
+        <div class="input-enLigne-createChapitre">
+            <label for="media">Media</label>
+            <input type="text" name="media" id="media"
+                   value="{{ old('media') }}"
+                   placeholder="Média lié au chapitre">
         </div>
-    @endif
-    <div>
-        <button class="btn btn-success" type="submit">Valide</button>
-    </div>
-</form>
+
+        <div class="input-enLigne-createChapitre">
+            <label for="question">Question</label>
+            <input type="text" name="question" id="question"
+                   value="{{ old('question') }}"
+                   placeholder="Question liée au chapitre">
+        </div>
+
+        @if(!isset($affichePremier))
+            <div>
+                <label for="premier">Premier chapitre?</label>
+                <input type="checkbox" name="premier" checked id="premier">
+            </div>
+        @endif
+        <div>
+            <button class="button-createChapitre" type="submit">Valider</button>
+        </div>
+
+    </form>
+
+</section>
+</div>
