0f8ea293d9fb017dcdaa551cd166dc6c179a8e46
Goudaillier Antoine <antoine_goudaillier@ens.univ-artois.fr>
2023-12-21 08:12:11 +0100

    ajout page login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,28 @@
 @extends("templates.app")
 
 @section('content')
+    <section class="section-register">
 
-    <form action="{{route("login")}}" method="post">
+    <form class="formulaire-logins" action="{{route("login")}}" method="post">
+
+        <h1>Se connecter</h1>
         @csrf
-        <input type="email" name="email" required placeholder="Email" /><br />
+        <div class="logins-inputs">
+            <div class="aff-row">
+                <label id="icon" for="email"><i class="fa-solid fa-envelope"></i></label>
+            <input type="email" name="email" required placeholder="Email" /><br />
+            </div>
+            <div class="aff-row">
+                <label id="icon" for="password"><i class="fa-solid fa-lock"></i></label>
         <input type="password" name="password" required placeholder="password" /><br />
-        Remember me<input type="checkbox" name="remember"   /><br />
-        <input type="submit" /><br />
+            </div>
+            <div class="aff-row">
+
+            Remember me<input type="checkbox" name="remember"   /><br />
+            </div>
+        <!--<input type="submit" /><br />-->
+                <button class="register-send" type="submit">Envoyer</button>
+
+        </div>
     </form>
 @endsection
