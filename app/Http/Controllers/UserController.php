ee2d8954f90ba5c5f2769daff94e144b951b2358
Esteban DAUCHY <esteban_dauchy@ens.univ-artois.fr>
2023-12-21 14:38:04 +0100

    Création du UserController pour modifier l'avatar

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function updateAvatar(Request $request)
+    {
+        if ($request->hasFile('avatar')) {
+            $avatarPath = $request->file('avatar')->store('avatars', 'public');
+            Auth::user()->update(['avatar' => $avatarPath]);
+            Auth::user()->save();
+        }
+
+        return redirect()->back()->with('success', 'Avatar mis à jour avec succès.');
+    }
+}
