979898329e373000963de2ffb29c19afd7ac18a3
thomashazebrouck <hazebrouck.thomas@hotmail.fr>
2023-12-21 11:40:06 +0100

    ajout page mentions légales

---
+++ resources/views/mentions.blade.php
@@ -0,0 +1,26 @@
+@extends("templates.app")
+
+@section('content')
+    <div style="display: flex;align-items: center; justify-content: center">
+        <div class='mention-fond'>
+            <div class="mention-container">
+            <H1>Mentions légales</H1>
+            <h3>Éditeur du site</h3>
+                <p>Nom du site : Le Panthéon</p>
+
+                <p>Adresse du site : <a href="http://172.31.146.106/~but23_groupe12/">http://172.31.146.106/~but23_groupe12/</a></p>
+
+
+<h3>Propriété intellectuelle</h3>
+<p>Le contenu de ce site (textes, images, vidéos, etc.) est la propriété exclusive de "Le Panthéon" et est protégé par les lois sur la propriété intellectuelle en vigueur. Toute reproduction ou utilisation sans autorisation préalable est strictement interdite.</p>
+
+<h3>Responsabilité</h3>
+<p>Les informations présentées sur ce site sont fournies à titre informatif et éducatif uniquement. "Le Panthéon" décline toute responsabilité quant à l'exactitude, l'exhaustivité ou la pertinence des informations fournies.</p>
+
+<h3>Cookies</h3>
+<p>Ce site n'utilise pas de cookies pour collecter des informations personnelles. Les cookies utilisés sont uniquement à des fins techniques et de navigation.</p>
+        </div>
+        </div>
+    </div>
+
+@endsection
