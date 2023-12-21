642302289eda6c468da6e8689c4f86a93860118b
Goudaillier Antoine <antoine_goudaillier@ens.univ-artois.fr>
2023-12-21 13:45:50 +0100

    debut style

--- resources/views/histoire/encours.blade.php
+++ resources/views/histoire/encours.blade.php
@@ -1,40 +1,43 @@
 @extends("templates.app")
 
 @section('content')
-    <h3>Création de {{$histoire->titre}}</h3>
+    <section class="container-encours">
+
+        <h1>Création de {{$histoire->titre}}</h1>
 
 
-   <div  style="width:45%;"> @include("histoire.createChapitre", ['id' => $histoire->id])
+       <div  style="width:45%;"> @include("histoire.createChapitre", ['id' => $histoire->id])
 
-    <ul>
+        <ul>
 
-    @foreach($histoire->chapitres as $c)
-        <li>{{$c->id }} : {{$c->titrecourt}}</li>
-    @endforeach
-    </ul>
-   </div>
-        <div>
-            <h3>Liaison des chapitres</h3>
-            <hr class="mt-2 mb-2">
-            <form method="post" action="{{route('lier', $histoire->id)}}">
-                @csrf
-                <p>Origine</p>
-                <select name="origine" >
-                    @foreach($histoire->chapitres as $c)
-                        <option value="{{$c->id }}">  {{$c->titrecourt}}</option>
-                    @endforeach
-                </select>
-                <p>Destination</p>
-                <select name="destination">
-                    @foreach($histoire->chapitres as $c)
-                        <option value="{{$c->id }}">  {{$c->titrecourt}}</option>
-                    @endforeach
-                </select>
-                <input type="text" name="reponse">
-                <button class="btn btn-success" type="submit">Lier</button>
-            </form>
+        @foreach($histoire->chapitres as $c)
+            <li>{{$c->id }} : {{$c->titrecourt}}</li>
+        @endforeach
+        </ul>
+       </div>
+            <div>
+                <h3>Liaison des chapitres</h3>
+                <hr class="mt-2 mb-2">
+                <form method="post" action="{{route('lier', $histoire->id)}}">
+                    @csrf
+                    <p>Origine</p>
+                    <select name="origine" >
+                        @foreach($histoire->chapitres as $c)
+                            <option value="{{$c->id }}">  {{$c->titrecourt}}</option>
+                        @endforeach
+                    </select>
+                    <p>Destination</p>
+                    <select name="destination">
+                        @foreach($histoire->chapitres as $c)
+                            <option value="{{$c->id }}">  {{$c->titrecourt}}</option>
+                        @endforeach
+                    </select>
+                    <input type="text" name="reponse">
+                    <button class="btn btn-success" type="submit">Lier</button>
+                </form>
 
 
+            </div>
         </div>
-    </div>
+    </section>
 @endsection
